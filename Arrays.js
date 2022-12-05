f3912e6f7e3b0eaea2f145a3c2220bb10697e769
themertzy <58080566+themertzy@users.noreply.github.com>
2022-12-05 14:50:55 -0600

    Update Arrays.js

--- Arrays.js
+++ Arrays.js
@@ -1,7 +1,21 @@
 function arrayMax(arr) {
-  return Math.max.apply(null, arr);
+  var len = arr.length;
+  var max = -Infinity;
+  while (len--) {
+    if (arr[len] > max) {
+      max = arr[len];
+    }
+  }
+  return max;
 }
 
 function arrayMin(arr) {
-  return Math.min.apply(null, arr);
+  var len = arr.length;
+  var min = Infinity;
+  while (len--) {
+    if (arr[len] < min) {
+      min = arr[len];
+    }
+  }
+  return min;
 }
